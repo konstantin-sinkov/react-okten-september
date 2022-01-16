1c68d665355e9ee38b44288a8be399c54d1b495b
konstantin-sinkov <sinkov.ks@gmail.com>
2022-01-16 23:17:04 +0200

    Lesson_2-2(cw) onClick filtration

--- src/App.js
+++ src/App.js
@@ -7,17 +7,37 @@ import {usersService} from "./services/users.service";
 
 function App() {
     const [users, setUsers] = useState([]);
+    const [filteredUsers, setFilteredUsers] = useState([]);
     
     useEffect(() => {
         usersService.getUsers()
-            .then(value => setUsers(value));
-    })
+            .then(value => {
+                setUsers([...value])
+                setFilteredUsers([...value])
+            });
+    });
     
+    //filtration function
+    const getFilter = (data) => {
+        let filteredArr = [...users];
+    
+        //comparing data from filled form input with each user
+        if (data.name) {
+            filteredArr = filteredArr.filter(user => user.name.toLowerCase().includes(data.name.toLowerCase()));
+        }
+        if (data.username) {
+            filteredArr = filteredArr.filter(user => user.username.toLowerCase().includes(data.username.toLowerCase()));
+        }
+        if (data.email) {
+            filteredArr = filteredArr.filter(user => user.email.toLowerCase().includes(data.email.toLowerCase()));
+        }
+        setFilteredUsers(filteredArr);
+    }
     
     return (
         <div className="App">
-            <Form />
-            <Users users={users}/>
+            <Form getFilter={getFilter}/>
+            <Users users={filteredUsers}/>
         </div>
     );
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Form = () => {
+const Form = ({getFilter}) => {
+    const [form, setForm] = useState({name: '', username: '', email: ''});
+    
+    //function creates object with data from form input values
+    const formHandler = (e) => {
+        const eventData = {...form, [e.target.name]: e.target.value};//dynamically key
+        setForm({...form, ...eventData});
+        getFilter(eventData);
+    }
+    
     return (
         <div>
-            <label><input type="text" name={"name"} /></label>
-            <label><input type="text" name={"username"} /></label>
-            <label><input type="text" name={"email"} /></label>
+            <label><input type="text" name={"name"} value={form.name} onChange={formHandler}/></label>
+            <label><input type="text" name={"username"} value={form.username} onChange={formHandler} /></label>
+            <label><input type="text" name={"email"} value={form.email} onChange={formHandler}/></label>
         </div>
     );
 }
