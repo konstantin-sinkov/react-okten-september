bd611032cd317885444e55efabd5ab91ba312446
konstantin-sinkov <sinkov.ks@gmail.com>
2022-01-26 16:37:19 +0200

    lesson_3-3(additional) - some changes to ImageBlock.js

--- src/components/ImageBlock/ImageBlock.js
+++ src/components/ImageBlock/ImageBlock.js
@@ -4,21 +4,13 @@ import {UncachedImage} from "react-uncached-image";
 
 const ImageBlock = ({imageURL}) => {
     // debugger;
-    // let [newImageURL, setNewImageURL] = useState(imageURL);
-    // let [counter, setCounter] = useState(0);
-    //
-    // useEffect((newImageURL) => {
-    //     setNewImageURL(newImageURL.space('').join(''));
-    // },[counter]);
-    //
-    // const updateImage = (counter) => {
-    //     setCounter(counter++);
-    // }
+    const updateImage = () => {
+    
+    }
     
     return (
         <div>
-            {/*<button onClick={updateImage}>*/}
-            <button>
+            <button onClick={updateImage}>
                 Update
             </button>
             <UncachedImage src={imageURL}/>
